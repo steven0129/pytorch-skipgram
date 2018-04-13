94a90c03fff2b246a3291238a1a04befda158186
steven0129 <h3584935@gmail.com>
2018-04-13 22:22:06 +0800

    Encode label

--- WhiteSnake/Vocabulary.py
+++ WhiteSnake/Vocabulary.py
@@ -4,21 +4,26 @@ import numpy as np
 import pickle
 import csv
 import os
+import functools
 
 class Dataset(data.Dataset):
     def __init__(self, ratio=0.6, windowSize=3):
         currentDir = os.path.dirname(os.path.abspath(__file__))
         data = list(csv.reader(open('{}/white-snake-preprocessor.csv'.format(currentDir), 'r')))
         data = sum(data[:int(len(data)*ratio)], [])
+        data = list(''.join(data))
+        data = list(filter(lambda x: x != 'n', data))
+        data = [x + 'n' if x == '\\' else x for x in data]
 
         self.windowSize = windowSize
         self.labelEncoder = pickle.load(open('{}/label.pickle'.format(currentDir), 'rb'))
         self.lengths = list(map(lambda x: len(x), data))
-        self.data = list(''.join(data))
+        self.data = data
 
     def __getitem__(self, index):
         window = [self.data[index + i] for i in range(self.windowSize)]
-        return tuple(window)
+        labels = self.labelEncoder.transform(window)
+        return tuple(labels)
 
     def __len__(self):
         return len(self.data) - (self.windowSize - 1)
