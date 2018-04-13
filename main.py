f92ab4dc7cedaf9f280e91b199017ea08408631a
steven0129 <h3584935@gmail.com>
2018-04-13 23:30:37 +0800

    Convert into tenosr and batch

--- main.py
+++ main.py
@@ -11,6 +11,19 @@ def train(**kwargs):
         setattr(options,k_,v_)
     
     whiteSnake = Dataset(ratio=options.ratio, windowSize=options.window_size)
+    X = []
+    Y = []
+    for pairs in whiteSnake:
+        for pair in list(pairs):
+            X.append(pair[0])
+            Y.append(pair[1])
+    
+    dataset = Data.TensorDataset(data_tensor=torch.Tensor(X), target_tensor=torch.Tensor(Y))
+    loader = Data.DataLoader(dataset=dataset, batch_size=options.batch_size, shuffle=True, drop_last=True, num_workers=options.core)
+    
+    for epoch in tqdm(range(options.epochs)):
+        for batchX, batchY in loader:
+            tqdm.write(str(batchX.numpy()) + ', ' + str(batchY.numpy()))
 
 if __name__=='__main__':
     import fire
