ede49b86613d56cba3ed67ed023b5c8b137c10e7
steven0129 <h3584935@gmail.com>
2018-04-14 00:03:38 +0800

    整理Code

--- WhiteSnake/Vocabulary.py
+++ WhiteSnake/Vocabulary.py
@@ -6,25 +6,32 @@ import csv
 import os
 import functools
 
+
 class Dataset(data.Dataset):
     def __init__(self, ratio=0.6, windowSize=2):
         currentDir = os.path.dirname(os.path.abspath(__file__))
-        data = list(csv.reader(open('{}/white-snake-preprocessor.csv'.format(currentDir), 'r')))
+        data = list(
+            csv.reader(
+                open(
+                    '{}/white-snake-preprocessor.csv'.format(currentDir),
+                    'r')))
         data = sum(data[:int(len(data)*ratio)], [])
         data = list(''.join(data))
         data = list(filter(lambda x: x != 'n', data))
         data = [x + 'n' if x == '\\' else x for x in data]
 
         self.windowSize = windowSize
-        self.labelEncoder = pickle.load(open('{}/label.pickle'.format(currentDir), 'rb'))
+        self.labelEncoder = pickle.load(
+            open('{}/label.pickle'.format(currentDir), 'rb'))
         self.lengths = list(map(lambda x: len(x), data))
         self.data = data
 
     def __getitem__(self, index):
         window = [self.data[index + i] for i in range(self.windowSize)]
         labels = self.labelEncoder.transform(window).tolist()
-        item = [(labels[i], labels[i + 1]) for i in range(len(labels) - 1)] if not self.windowSize == 2 else [] # 兩兩配對
-        item.append((labels[-1], labels[0])) # 第一與最後配對
+        item = [(labels[i], labels[i + 1]) for i in range(len(labels) - 1)
+                ] if not self.windowSize == 2 else []  # 兩兩配對
+        item.append((labels[-1], labels[0]))  # 第一與最後配對
         return tuple(item)
 
     def __len__(self):
--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import torch.utils.data as Data
 
 options = Env()
 
+
 def train(**kwargs):
-    for k_,v_ in kwargs.items():
-        setattr(options,k_,v_)
-    
+    for k_, v_ in kwargs.items():
+        setattr(options, k_, v_)
+
     whiteSnake = Dataset(ratio=options.ratio, windowSize=options.window_size)
     X = []
     Y = []
@@ -17,14 +18,22 @@ def train(**kwargs):
         for pair in list(pairs):
             X.append(pair[0])
             Y.append(pair[1])
-    
-    dataset = Data.TensorDataset(data_tensor=torch.Tensor(X), target_tensor=torch.Tensor(Y))
-    loader = Data.DataLoader(dataset=dataset, batch_size=options.batch_size, shuffle=True, drop_last=True, num_workers=options.core)
-    
+
+    dataset = Data.TensorDataset(
+        data_tensor=torch.Tensor(X),
+        target_tensor=torch.Tensor(Y))
+    loader = Data.DataLoader(
+        dataset=dataset,
+        batch_size=options.batch_size,
+        shuffle=True,
+        drop_last=True,
+        num_workers=options.core)
+
     for epoch in tqdm(range(options.epochs)):
         for batchX, batchY in loader:
             tqdm.write(str(batchX.numpy()) + ', ' + str(batchY.numpy()))
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     import fire
     fire.Fire()
