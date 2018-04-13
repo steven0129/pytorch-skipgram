625a7bc78b0148285aed23e2e070c8960ffb698a
steven0129 <h3584935@gmail.com>
2018-04-13 23:01:52 +0800

    兩兩配對window裡面的字

--- WhiteSnake/Vocabulary.py
+++ WhiteSnake/Vocabulary.py
@@ -7,7 +7,7 @@ import os
 import functools
 
 class Dataset(data.Dataset):
-    def __init__(self, ratio=0.6, windowSize=3):
+    def __init__(self, ratio=0.6, windowSize=2):
         currentDir = os.path.dirname(os.path.abspath(__file__))
         data = list(csv.reader(open('{}/white-snake-preprocessor.csv'.format(currentDir), 'r')))
         data = sum(data[:int(len(data)*ratio)], [])
@@ -21,9 +21,11 @@ class Dataset(data.Dataset):
         self.data = data
 
     def __getitem__(self, index):
-        window = [self.data[index + i] for i in range(self.windowSize)]
-        labels = self.labelEncoder.transform(window)
-        return tuple(labels)
+        elements = [self.data[index + i] for i in range(self.windowSize)]
+        labels = self.labelEncoder.transform(elements).tolist()
+        item = [(labels[i], labels[i + 1]) for i in range(len(labels) - 1)] if not self.windowSize == 2 else [] # 兩兩配對
+        item.append((labels[-1], labels[0])) # 第一與最後配對
+        return tuple(item)
 
     def __len__(self):
         return len(self.data) - (self.windowSize - 1)
